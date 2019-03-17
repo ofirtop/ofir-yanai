a796d4100d52c55994c7774c86fdd18e8ecd88d2
ofirtop <ofirtopaz@gmail.com>
2019-03-17 10:23:00 +0200

    FIX SOMTHING...

---
+++ js/game.js
@@ -0,0 +1,79 @@
+'use strict';
+var WALL = '#';
+var FOOD = '.';
+var EMPTY = ' ';
+
+var gBoard;
+var gGame = {
+  score: 0,
+  isOn: false
+};
+
+function init() {
+  gBoard = buildBoard();
+
+  createPacman(gBoard);
+  createGhosts(gBoard);
+
+  printMat(gBoard, '.board-container');
+  // console.table(gBoard);
+  gGame.isOn = true;
+}
+
+function buildBoard() {
+  var SIZE = 10;
+  var board = [];
+  for (var i = 0; i < SIZE; i++) {
+    board.push([]);
+    for (var j = 0; j < SIZE; j++) {
+      board[i][j] = FOOD;
+
+      if (i === 0 || i === SIZE - 1 ||
+        j === 0 || j === SIZE - 1 ||
+        (j === 3 && i > 4 && i < SIZE - 2)) {
+
+        board[i][j] = WALL;
+      }
+    }
+  }
+  return board;
+}
+
+function updateScore(value) {
+  // Update both the model and the dom for the score
+  gGame.score += value;
+  document.querySelector('header h3 span').innerText = gGame.score;
+}
+
+function printMat(mat, selector) {
+  var strHTML = '<table border="0"><tbody>';
+  for (var i = 0; i < mat.length; i++) {
+    strHTML += '<tr>';
+    for (var j = 0; j < mat[0].length; j++) {
+      var cell = mat[i][j];
+      var className = 'cell cell' + i + '-' + j;
+      strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+    }
+    strHTML += '</tr>'
+  }
+  strHTML += '</tbody></table>';
+  var elContainer = document.querySelector(selector);
+  elContainer.innerHTML = strHTML;
+}
+
+function renderCell(location, value) {
+  // Select the elCell and set the value
+  var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
+  elCell.innerHTML = value;
+}
+
+function gameOver() {
+  console.log('Game Over');
+  gGame.isOn = false;
+  clearInterval(gIntervalGhosts);
+  gIntervalGhosts = 0;
+}
+
+
+
+
